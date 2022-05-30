0f22229fc1900ac4df48a6bfbbdd939fb951e6ec
KloopRE <100780612+KloopRE@users.noreply.github.com>
2022-05-30 19:04:50 +0300

    .
    ж

---
+++ client/form3.js
@@ -0,0 +1,30 @@
+
+const form_handle = async (event) => {  //Создаем ассинхронный callback, чтобы при событии 'submit' страница не перезагружалась
+    event.preventDefault() // Если событие не обрабатывается явно, то оно не должно вести себя обычно
+    const request = await fetch('/form3', { // создаем fetch обьект, которыйы может отправлять сетевые запросы на сервер и подгружать инфу.
+        // в него передаем URL формы
+        method: 'POST', // Метод 'отправить'
+        headers: { //Заголовки, чтобы сервер понимал тип обрабатываемой информации
+            'Content-Type': 'application/json', //Информация контента json
+            'Accept': 'application/json', //Принающая инфа. json
+        },
+
+        body: JSON.stringify(Object.fromEntries((new FormData(event.target)).entries()))
+        // event.target - наша форма из обьекта fetch, c помощью конструктора new FormData мы считываем с нашей форма данные
+        // т.е. происходит отправка формы. // преобразуем форму в обьект данных с помощью fromEntries и возвращаем массив
+        // перечисляемых пар свойств c помощью entries. Преобразуем массив в json формат с помощью stringify
+    });
+    const response = await request.json(); // Ждем ответ от запроса
+    if (!request.ok) {
+        alert(response.errors); // если его нет, то ошибка
+    } else {
+        alert(response)
+        // alert("Вы успешно вошли в свой аккаунт.")
+    }
+}
+
+
+document.querySelector('form').addEventListener('submit', form_handle); // В документе выбираем обьект "форма", добавляем слушателя событий, события
+// 'submit' каллбек 'form_handle'
+
+
--- test_server/server.js
+++ test_server/server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
+
+
+
 
 export const serv = express();
 const PORT = 1007;
 
-serv.use(express.static(path.resolve('..', 'client')));// выбираем статический ресурс по его абсолютному пути
+serv.use(express.static(path.resolve('.', 'client')));// выбираем статический ресурс по его абсолютному пути
 serv.use(express.json());// анализатор тела с входящими json запросами
 serv.use(express.urlencoded({extended: true}));// кодируем обьект в формате URL
 
 
-serv.get('/', (request, response) => {response.sendFile(path.resolve('..', 'client', 'index.html')) //Отправляем ответ серверу в виде html файла
+serv.get('/', (request, response) => {response.sendFile(path.resolve('.', 'client', 'index.html')) //Отправляем ответ серверу в виде html файла
 })
 
 serv.listen(PORT, () => {
@@ -17,17 +21,67 @@ serv.listen(PORT, () => {
 })
 
 
-serv.get('/registration', (request, response) => {response.sendFile(path.resolve('..', 'client', 'registration.html')) //Отправляем ответ серверу в виде html файла
+
+
+
+serv.get('/', (request, response) => {response.sendFile(path.resolve('.', 'client', 'login.html')) //Отправляем ответ серверу в виде html файла
 })
+
+serv.post('/form3', (request, response) => {
+    const {adr, zag, text} = request.body;
+    response.status(200).json(`${adr} ${zag} ${text}`)
+    let filename = "./database/messager.json";
+    fs.readFile(filename, 'utf8', (err,data) => {
+        if (err){console.error(err);
+            return;}
+        let jsonData = JSON.parse(data);
+
+        let nextId = jsonData.message[jsonData.message.length -1].id+1
+
+        jsonData.message.push({"userid": 1,"adr": adr, "zag": zag, "text": text});
+        console.log(jsonData);
+        fs.writeFile(filename, JSON.stringify(jsonData), (err) =>{
+            if (err) {console.error(err)}
+        });
+    })
+})
+
 serv.post('/form2', (request, response) => {
     const {login, password} = request.body
     response.status(200).json(`${login} ${password}`)
+    let filename = "./database/users.json";
+    fs.readFile(filename, 'utf8', (err,data) => {
+        if (err){console.error(err);
+            return;}
+        let jsonData = JSON.parse(data);
+
+        let nextId = jsonData.user[jsonData.user.length -1].id+1
+
+        jsonData.user.push({"id": nextId,"username": login, "password": password});
+        console.log(jsonData);
+        fs.writeFile(filename, JSON.stringify(jsonData), (err) =>{
+            if (err) {console.error(err)}
+        });
+    })
 })
 
-serv.get('/login', (request, response) => {response.sendFile(path.resolve('..', 'client', 'login.html')) //Отправляем ответ серверу в виде html файла
-})
 serv.post('/form', (request, response) => {
     const {login, password} = request.body
     response.status(200).json(`${login} ${password}`)
+
+    let filename = "./database/users.json";
+    const data = JSON.parse(fs.readFileSync(filename,"utf-8"));
+
+    for(let i = 0; i < data.user.length; i++)
+    {
+        if(login === data.user[i].username && password === data.user[i].password)
+        {
+            console.log("Пользователь вошел в аккаунт");
+        }
+        else {console.log("Пользователь не вошел в аккаунт"); return;}
+    }
+
+
+
 })
 
